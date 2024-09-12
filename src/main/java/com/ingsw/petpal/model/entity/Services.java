701190820f763c213dca9ecdb9ce031d54991e56
Nayeli Mallma <111675030+NayeliMaVi@users.noreply.github.com>
2024-09-11 21:38:24 -0500

    CopyPaste Arreglado

--- src/main/java/com/ingsw/petpal/model/entity/Services.java
+++ src/main/java/com/ingsw/petpal/model/entity/Services.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 
 @Data
 @Entity
-@Table(name="cuidadores")
+@Table(name="servicios")
 public class Services {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
