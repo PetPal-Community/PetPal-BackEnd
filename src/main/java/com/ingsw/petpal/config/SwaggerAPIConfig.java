f5dd7877d75c5f2284ed9cfbaf17d0b194aa3f3c
Nayeli Mallma <nmallma91@gmail.com>
2024-10-24 14:35:25 -0500

    actualizando antes del merge gmail

--- src/main/java/com/ingsw/petpal/config/SwaggerAPIConfig.java
+++ src/main/java/com/ingsw/petpal/config/SwaggerAPIConfig.java
@@ -1,47 +1,71 @@
 package com.ingsw.petpal.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
+// Asegurando que se haya hecho merge correctamente
 @Configuration
 public class SwaggerAPIConfig {
 
-    // Puedes establecer la URL de tu servidor de desarrollo en application.properties
-    @Value("http://localhost:8080/api/v2")
+    @Value("${petpal.openapi.dev-url}")
     private String devUrl;
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI getOpenAPI() {
         // Definir el servidor de desarrollo
         Server devServer = new Server();
         devServer.setUrl(devUrl);
-        devServer.setDescription("Servidor de desarrollo para PetPal API");
+        devServer.setDescription("This is the Dev Server");
 
-        // Información de contacto del desarrollador o responsable de la API
+        // Informacion de Contacto
         Contact contact = new Contact();
-        contact.setName("PetPal Team");
-        contact.setEmail("contacto@petpal.com");
-        contact.setUrl("https://github.com/PetPal-Community/PetPal-BackEnd");
+        contact.setName("petpal-community");
+        contact.setEmail("petpalcommnutiy@gmail.com");
+        contact.setUrl("https://github.com/ingsw/petpal");
 
-        // Definir la licencia de la API (puedes cambiarla según tus necesidades)
         License mitLicense = new License().name("MIT License").url("https://opensource.org/licenses/MIT");
 
-        // Información general sobre la API
-        Info apiInfo = new Info()
-                .title("PetPal API")
+        // Informacion General de la API
+        Info info = new Info()
+                .title("API petpal")
                 .version("1.0")
                 .contact(contact)
-                .description("API para la gestión de mascotas, recordatorios y más en PetPal")
-                .termsOfService("https://github.com/PetPal-Community/PetPal-BackEnd/terms")
+                .description("API de petpal community")
+                .termsOfService("https://github.com/ingsw/test-invoice/terms")
                 .license(mitLicense);
 
-        // Crear y retornar el objeto OpenAPI con la configuración
-        return new OpenAPI().info(apiInfo).addServersItem(devServer);
+
+        // Configuración de seguridad JWT
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("JWT Authentication");
+
+        Components components = new Components()
+                .addSecuritySchemes("bearerAuth", securityScheme);
+
+        // Requerimiento de seguridad para utilizar en las operaciones
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+
+
+        return new OpenAPI()
+                .info(info)
+                //.addServersItem(devServer);
+                .servers(List.of(devServer))
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
+
 }
